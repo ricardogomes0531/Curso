343e752f78f082c1d0739afd0b5add0f0c365b23
Jose Ricardo Gomes de Oliveira Junior <jose.oliveira-ext@viavarejo.com.br>
2021-01-07 17:28:33 -0300

    Cadastro de usuário agora verifica se e-mail já existe.

--- I9Solucoes/I9Solucoes/Controllers/CadastroController.cs
+++ I9Solucoes/I9Solucoes/Controllers/CadastroController.cs
@@ -22,27 +22,38 @@ namespace I9Solucoes.Controllers
         {
             CadastroRepository cadastro = new CadastroRepository();
             bool resultado = false;
+            int idAluno = 0;
             Erro erro = new Erro();
             try
             {
-                resultado = cadastro.Inserir(Request.Form["nome"].ToString(), Convert.ToDateTime(Request.Form["dataNascimento"].ToString()), Request.Form["cpf"].ToString(), Request.Form["sexo"].ToString(), Request.Form["email"].ToString(), Request.Form["celular"].ToString(), Request.Form["whatsapp"].ToString(), Request.Form["senha"].ToString());
-                if (resultado)
+                idAluno = new UsuarioRepository().PesquisarIdDoAlunoPeloEmail(Request.Form["email"].ToString());
+                if (idAluno > 0)
                 {
-                    erro.Mensagem = "Cadastro realizado com sucesso";
+                    erro.Mensagem = "O e-mail informado já está em uso no sistema.";
                     erro.Detalhe = null;
-                    erro.ExisteErro =false;
-                    Mail.Enviar(Request.Form["email"].ToString(),"Cadastro no Portal Visão de DEV", ConfigurationManager.AppSettings["MailMensagemBemVindo"].ToString());
-                    Mail.Enviar(ConfigurationManager.AppSettings["MailEmailAdministrador"].ToString(),"Cadastro de Novo Aluno no Site Visão de DEV","Um novo aluno(a) se cadastrou no site Visão de DEV. O e-mail é "+Request.Form["email"].ToString()+" o nome é "+Request.Form["nome"].ToString()+" o celular cadastrado é: "+Request.Form["celular"].ToString()+", é whatsapp: "+Request.Form["whatsapp"].ToString());
+                    erro.ExisteErro = true;
                 }
                 else
                 {
-                    erro.Mensagem = "Erro ao realizar o cadastro.";
-                    erro.Detalhe = null;
-                    erro.ExisteErro = true;
-                    Mail.Enviar(ConfigurationManager.AppSettings["MailEmailAdministrador"].ToString(),"Erro no Cadastro de Novo Aluno no Site Visão de DEV","Um novo aluno(a) obteve um erro ao se cadastrar no site Visão de DEV. O e-mail é "+Request.Form["email"].ToString()+" o nome é "+Request.Form["nome"].ToString()+" o celular cadastrado é: "+Request.Form["celular"].ToString()+", é whatsapp: "+Request.Form["whatsapp"].ToString()+" a data de nascimento é: "+Request.Form["dataNascimento"].ToString());
+                    resultado = cadastro.Inserir(Request.Form["nome"].ToString(), Convert.ToDateTime(Request.Form["dataNascimento"].ToString()), Request.Form["cpf"].ToString(), Request.Form["sexo"].ToString(), Request.Form["email"].ToString(), Request.Form["celular"].ToString(), Request.Form["whatsapp"].ToString(), Request.Form["senha"].ToString());
+                    if (resultado)
+                    {
+                        erro.Mensagem = "Cadastro realizado com sucesso";
+                        erro.Detalhe = null;
+                        erro.ExisteErro = false;
+                        Mail.Enviar(Request.Form["email"].ToString(), "Cadastro no Portal Visão de DEV", ConfigurationManager.AppSettings["MailMensagemBemVindo"].ToString());
+                        Mail.Enviar(ConfigurationManager.AppSettings["MailEmailAdministrador"].ToString(), "Cadastro de Novo Aluno no Site Visão de DEV", "Um novo aluno(a) se cadastrou no site Visão de DEV. O e-mail é " + Request.Form["email"].ToString() + " o nome é " + Request.Form["nome"].ToString() + " o celular cadastrado é: " + Request.Form["celular"].ToString() + ", é whatsapp: " + Request.Form["whatsapp"].ToString());
+                    }
+                    else
+                    {
+                        erro.Mensagem = "Erro ao realizar o cadastro.";
+                        erro.Detalhe = null;
+                        erro.ExisteErro = true;
+                        Mail.Enviar(ConfigurationManager.AppSettings["MailEmailAdministrador"].ToString(), "Erro no Cadastro de Novo Aluno no Site Visão de DEV", "Um novo aluno(a) obteve um erro ao se cadastrar no site Visão de DEV. O e-mail é " + Request.Form["email"].ToString() + " o nome é " + Request.Form["nome"].ToString() + " o celular cadastrado é: " + Request.Form["celular"].ToString() + ", é whatsapp: " + Request.Form["whatsapp"].ToString() + " a data de nascimento é: " + Request.Form["dataNascimento"].ToString());
+                    }
                 }
-            }
-            catch (Exception ex)
+                }
+                        catch (Exception ex)
             {
                 erro.Mensagem = "Erro ao realizar cadastro " + ex.Message;
                 erro.Detalhe = ex.Message;
